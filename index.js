0246a6d870199de43279e20aeac4172df49838ad
Etixen <94640738+Etixen@users.noreply.github.com>
2022-08-31 21:14:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
+    $(".silo_list")
+    $(".silo_names")
+    $("#map")
+  
 }
 
 function signOut() {
